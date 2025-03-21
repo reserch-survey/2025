700f7ff1b72a512be8114f4422cd19f706d18e09
cai23433 <caixingtong21@gmail.com>
2025-03-21 19:46:55 +0800

    unboxing

--- url.js
+++ url.js
@@ -1,13 +1,6 @@
-/*
- * 將要隨機派發的網址放入下方（不需排序）
- *
- * 並請注意：
- * 1. 網址請用引號（或稱「撇號」，單引號或雙引號皆可）包起來
- * 2. 包起來的網址之間用逗號分隔
- */
-
-
 const urls = [
-	'https://www.surveycake.com/s/966gq',
-	'https://www.surveycake.com/s/0zZA3',
+	'https://www.surveycake.com/s/46Z0g',
+	'https://www.surveycake.com/s/76Gbv',
+	'https://www.surveycake.com/s/86GY1',
+	'https://www.surveycake.com/s/46Z0g',
 ];
